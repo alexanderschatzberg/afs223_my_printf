16ef110918e3e6ecaece21e18b6de3f24fd1410b
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-08-31 23:26:17 +0000

    Add tests for print_integer

--- test_my_printf.c
+++ test_my_printf.c
@@ -1,25 +1,39 @@
 #include "my_printf.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
   // PRINT INTEGER TESTS
-  // TODO: Add more tests
   printf("----------------------------");
   printf("\nPRINT INTEGER TESTS:\n");
-  print_integer(768336, 10, ""); // Test 1
+  print_integer(3410, 10, ""); // should print “3410”
   fputc('\n', stdout);
-  print_integer(-768336, 10, ""); // Test 2
+  print_integer(-3410, 10, ""); // should print “ - 3410”
   fputc('\n', stdout);
-  print_integer(-768336, 10, "$"); // Test 3, etc.
+  print_integer(-3410, 10, "$"); // should print “ - $3410
+  fputc('\n', stdout);
+  print_integer(3410, 16, ""); // should print “d52”
+  fputc('\n', stdout);
+  print_integer(3410, 16, "0x"); // should print “0xd52”
+  fputc('\n', stdout);
+  print_integer(-3410, 2, "0b"); // should print “0b11111111111111111111001010101110”
+  fputc('\n', stdout);
+  print_integer(3410, 2, "0b"); // should print “0b110101010010”
+  fputc('\n', stdout);
+  print_integer(-3410, 16, "0x"); // should print “0xfffff2ae”
+  fputc('\n', stdout);
+  print_integer(0, 10, ""); // should print 0
+  fputc('\n', stdout);
+  print_integer(-0, 10, ""); // should print 0
+  fputc('\n', stdout);
+  print_integer(UINT_MAX, 10, ""); // should print -1 
   fputc('\n', stdout);
 
   // MY_PRINTF TESTS
   // TODO: Add more tests
-  printf("----------------------------");
-  printf("\nMY_PRINTF TESTS:\n");
-  my_printf("768336\n");                                  // Test 1
-  my_printf("My favorite number is %d!\n", 768336);       // Test 2
-  my_printf("%d written in hex is %x\n", 768336, 768336); // Test 3, etc.
+  // printf("----------------------------");
+  // printf("\nMY_PRINTF TESTS:\n");
+  // my_printf("768336\n");                                  // Test 1
+  // my_printf("My favorite number is %d!\n", 768336);       // Test 2
+  // my_printf("%d written in hex is %x\n", 768336, 768336); // Test 3, etc.
 
   return EXIT_SUCCESS;
 }
